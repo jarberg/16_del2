864dd3f40b1ea59c7536d6661367c00eaa52f29e
jarberg <mjarberg88@gmail.com>
2018-11-08 10:42:07 +0100

    made test folder to upload and updated maven dependencies

---
+++ src/test/java/DTU/SWT_grp16/Controller/GameControllerTest.java
@@ -0,0 +1,15 @@
+package DTU.SWT_grp16.Controller;
+
+
+import org.junit.jupiter.api.Test;
+
+class GameControllerTest {
+
+    @Test
+    void setupGame() {
+    }
+
+    @Test
+    void playGame() {
+    }
+}
