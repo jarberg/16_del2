c267e722b9dec0dad6356d519a670c78a459fa56
jarberg <mjarberg88@gmail.com>
2018-11-08 18:09:44 +0100

    removed old field

---
+++ src/test/java/DTU/SWT_grp16/Application/MainTest.java
@@ -0,0 +1,12 @@
+package DTU.SWT_grp16.Application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+    }
+}
