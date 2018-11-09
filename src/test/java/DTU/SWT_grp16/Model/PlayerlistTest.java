506faebd6d78e9f8d3389fcf74ee44f9d999f20e
jarberg <mjarberg88@gmail.com>
2018-11-09 10:04:57 +0100

    added negativ tests

--- src/test/java/DTU/SWT_grp16/Model/PlayerlistTest.java
+++ src/test/java/DTU/SWT_grp16/Model/PlayerlistTest.java
@@ -7,16 +7,19 @@ class PlayerlistTest {
 
     String player1 ="bob";
     String player2 = "ida";
-    Playerlist playerList = new Playerlist(player1, player2);;
-
+    Playerlist playerList = new Playerlist(player1, player2);
 
     @Test
     void getCurrentPlayer() {
+        //positiv test
         assertEquals(player1, playerList.getCurrentPlayer().getName());
+        //negativ test
+        assertNotEquals(player2, playerList.getCurrentPlayer().getName());
     }
 
     @Test
     void changePlayerTurn() {
+        //positiv unit test
         assertEquals(player1, playerList.getCurrentPlayer().getName());
         playerList.changePlayerTurn();
         assertEquals(player2, playerList.getCurrentPlayer().getName());
@@ -26,5 +29,16 @@ class PlayerlistTest {
         assertEquals(player2, playerList.getCurrentPlayer().getName());
         playerList.changePlayerTurn();
         assertEquals(player1, playerList.getCurrentPlayer().getName());
+
+        //negativ unit test
+        assertNotEquals(player2, playerList.getCurrentPlayer().getName());
+        playerList.changePlayerTurn();
+        assertNotEquals(player1, playerList.getCurrentPlayer().getName());
+        playerList.changePlayerTurn();
+        assertNotEquals(player2, playerList.getCurrentPlayer().getName());
+        playerList.changePlayerTurn();
+        assertNotEquals(player1, playerList.getCurrentPlayer().getName());
+        playerList.changePlayerTurn();
+        assertNotEquals(player2, playerList.getCurrentPlayer().getName());
     }
 }
